a096ff4c42c88537598949bc86c7d5f32db87c68
Marko Nikolić <116906156+marko-nikolic01@users.noreply.github.com>
2024-11-13 22:29:56 +0100

    go: Working with wait groups.

--- Fakultet/PIDAISP/Vezbe/Vezbe4/WaitGroup/wait_group.go
+++ Fakultet/PIDAISP/Vezbe/Vezbe4/WaitGroup/wait_group.go
@@ -9,9 +9,9 @@ import (
 func main() {
 	var waitGroup sync.WaitGroup
 
-	waitGroup.Add(1000)
+	waitGroup.Add(10000)
 
-	for i := 1; i < 1001; i++ {
+	for i := 1; i < 10001; i++ {
 		go func() {
 			defer waitGroup.Done()
 			fmt.Printf("Go routine %d sleeping...\n", i)
